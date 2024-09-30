44d156d8202a7cc562a16bf5edb7c58f93b6919f
ryanjwong <ryanjwong007@gmail.com>
2024-09-30 14:37:58 -0700

    Task 5 done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function alertFunc() {
-    alert(document.getElementById("subscribe-form").email.value);
+    alert("Subscriber \"added\": " + document.getElementById("subscribe-form").email.value);
 }
 window.onload = function() {
     document.getElementById("subscribe-form").addEventListener("submit", alertFunc);
